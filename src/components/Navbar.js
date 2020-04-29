9d6d26db73c7d061002a7ebf2829037a9ec1a370
Sarah Brich <sarahleighbrich@gmail.com>
2020-04-29 13:20:51 -0400

    ok

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -64,16 +64,10 @@ const Navbar = class extends React.Component {
                 About
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
+                Offerings
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
